2d2c4c5aef2fff207914da08b39f4e8f3280ddd4
GayathriSrividya <gayathrirajavarapu@sanketika.in>
2022-10-11 17:08:18 +0530

    read events from kafka

--- src/kafka/kafka_consumer.py
+++ src/kafka/kafka_consumer.py
@@ -2,7 +2,6 @@
 from kafka import KafkaConsumer
 from json import loads
 
-
 # pulling events from kafka
 consumer = KafkaConsumer('sb-telemetry', bootstrap_servers=['localhost:9092'], group_id='my-group', value_deserializer=lambda x: loads(x.decode('utf-8')), consumer_timeout_ms=100000)
 
